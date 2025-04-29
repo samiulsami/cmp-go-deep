56a6a23d59a70e482f355ea65be0c9a4dede4553
Samiul Islam <samiulsami7786@gmail.com>
2025-04-30 01:29:59 +0600

    Handle duplicate package aliases, remove duplicate tags. (#2)

--- lua/cmp_go_deep/treesitter_implementations.lua
+++ lua/cmp_go_deep/treesitter_implementations.lua
@@ -24,13 +24,21 @@ local function get_root_node(bufnr)
 end
 
 ---@param bufnr (integer)
+---@param package_alias string | nil
 ---@param import_path string
-treesitter_implementations.add_import_statement = function(bufnr, import_path)
+treesitter_implementations.add_import_statement = function(bufnr, package_alias, import_path)
 	local root = get_root_node(bufnr)
 	if root == nil then
 		return
 	end
 
+	if not package_alias then
+		package_alias = ""
+	else
+		package_alias = package_alias .. " "
+	end
+
+	---@type TSNode | nil
 	local import_node = nil
 	for node in root:iter_children() do
 		if node:type() == "import_declaration" then
@@ -41,33 +49,32 @@ treesitter_implementations.add_import_statement = function(bufnr, import_path)
 
 	if import_node then
 		local start_row, _, end_row, _ = import_node:range()
-		local lines = vim.api.nvim_buf_get_lines(bufnr, start_row, end_row + 1, false)
-
 		if import_node:named_child_count() == 1 then
 			local child = import_node:named_child(0)
-			local type = child:type()
+			if not child then
+				vim.notify("could not parse import line with treesitter", vim.log.levels.WARN)
+				return
+			end
 
+			local type = child:type()
 			if type == "interpreted_string_literal" or type == "raw_string_literal" or type == "import_spec" then
-				local import_text = vim.treesitter.get_node_text(child, bufnr)
-				local indent = "\t"
-				local new_lines = {
+				vim.api.nvim_buf_set_lines(bufnr, start_row, end_row + 1, false, {
 					"import (",
-					indent .. import_text,
-					indent .. '"' .. import_path .. '"',
+					"\t" .. vim.treesitter.get_node_text(child, bufnr),
+					"\t" .. package_alias .. '"' .. import_path .. '"',
 					")",
-				}
-				vim.api.nvim_buf_set_lines(bufnr, start_row, end_row + 1, false, new_lines)
+				})
 				return
 			end
 		end
 
-		for i = #lines, 1, -1 do
-			if lines[i]:match("^%s*%)") then
-				table.insert(lines, i, '\t"' .. import_path .. '"')
-				break
-			end
+		local lines = vim.api.nvim_buf_get_lines(bufnr, start_row, end_row + 1, false)
+		if not lines[#lines]:match("^%s*%)") then
+			vim.notify("could not parse import block with treesitter", vim.log.levels.WARN)
+			return
 		end
 
+		table.insert(lines, #lines, "\t" .. package_alias .. '"' .. import_path .. '"')
 		vim.api.nvim_buf_set_lines(bufnr, start_row, end_row + 1, false, lines)
 	else
 		local insert_line = 0
@@ -81,21 +88,29 @@ treesitter_implementations.add_import_statement = function(bufnr, import_path)
 		vim.api.nvim_buf_set_lines(bufnr, insert_line, insert_line, false, {
 			"",
 			"import (",
-			'\t"' .. import_path .. '"',
+			"\t" .. package_alias .. '"' .. import_path .. '"',
 			")",
 			"",
 		})
 	end
 end
 
+---@param str string
+---@return string
+---@return integer count
+local function trim_quotes(str)
+	return str:gsub('^["`]+', ""):gsub('["`]+$', "")
+end
+
 ---@param bufnr (integer)
----@return table<string, boolean>
+---@return table<string, string> -- key: import path, value: package alias
 treesitter_implementations.get_imported_paths = function(bufnr)
 	local root = get_root_node(bufnr)
 	if root == nil then
 		return {}
 	end
 
+	---@type TSNode | nil
 	local import_node = nil
 	for i = 0, root:named_child_count() - 1 do
 		local node = root:named_child(i)
@@ -109,6 +124,23 @@ treesitter_implementations.get_imported_paths = function(bufnr)
 	end
 
 	local imported_paths = {}
+	---@param spec TSNode?
+	local process_import_spec = function(spec)
+		local path_node = spec and spec:field("path")[1]
+		local name_node = spec and spec:field("name")[1]
+		if path_node then
+			local text = vim.treesitter.get_node_text(path_node, bufnr)
+			if text then
+				text = trim_quotes(text)
+				local package_alias = name_node and vim.treesitter.get_node_text(name_node, bufnr)
+				package_alias = package_alias or text:match("([^/]+)$")
+				if package_alias == nil then
+					vim.notify("could not parse import line with treesitter " .. text, vim.log.levels.WARN)
+				end
+				imported_paths[text] = package_alias
+			end
+		end
+	end
 
 	for j = 0, import_node:named_child_count() - 1 do
 		local child = import_node:named_child(j)
@@ -117,29 +149,21 @@ treesitter_implementations.get_imported_paths = function(bufnr)
 		end
 
 		local type = child:type()
-
-		if type == "interpreted_string_literal" or type == "raw_string_literal" then
-			local text = vim.treesitter.get_node_text(child, bufnr)
-			text = text:gsub('^["`]+', ""):gsub('["`]+$', "")
-			imported_paths[text] = true
+		if type == "import_spec" and child:named_child_count() > 0 then -- single line import
+			process_import_spec(child:named_child(0))
 			goto continue
 		end
 
-		if type == "import_spec_list" then
-			for k = 0, child:named_child_count() - 1 do
-				local spec = child:named_child(k)
-				local path_node = spec and spec:field("path")[1]
-				if path_node then
-					local text = vim.treesitter.get_node_text(path_node, bufnr)
-					text = text:gsub('^["`]+', ""):gsub('["`]+$', "")
-					imported_paths[text] = true
-				end
+		if type == "import_spec_list" then -- multiline import
+			for k = 0, child:child_count() - 1 do
+				process_import_spec(child:named_child(k))
 			end
 			break
 		end
 
 		::continue::
 	end
+
 	return imported_paths
 end
 
