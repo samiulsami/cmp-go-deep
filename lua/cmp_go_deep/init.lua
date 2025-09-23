36657293af1c408aa094e424369e8b0104e0898e
sami <sami@appscode.com>
2025-09-23 17:17:54 +0600

    feat: block until db is initialized
    Signed-off-by: sami <sami@appscode.com>

--- lua/cmp_go_deep/init.lua
+++ lua/cmp_go_deep/init.lua
@@ -75,10 +75,10 @@ source.complete = function(_, params, callback)
 	callback({ items = {}, isIncomplete = true })
 
 	if not source.cache then
-		vim.schedule(function()
-			source.cache = require("cmp_go_deep.db").setup(source.opts)
-		end)
-                return callback({ items = {}, isIncomplete = true })
+		source.cache = require("cmp_go_deep.db").setup(source.opts)
+		if not source.cache then
+			return callback({ items = {}, isIncomplete = false })
+		end
 	end
 
 	if not gopls_requests.debounced_workspace_symbols then
